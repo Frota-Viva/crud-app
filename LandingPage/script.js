fb344b492794316862350c0077d53f82f52232bd
RicardoCiriloJunior <ricardocjr07@gmail.com>
2025-08-27 16:33:04 -0300

    Colocando a landing page no repositório

---
+++ LandingPage/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".carousel").forEach((carousel) => {
+        const avancar = carousel.querySelector(".avancar");
+        const voltar = carousel.querySelector(".voltar");
+        const cards = carousel.querySelectorAll(".carousel-card");
+        let i = 0;
+        const qtdCards = cards.length;
+
+        function moverCards(){
+            const distancia = (i * -1) * 120;
+            cards.forEach((card) => {
+                card.style.transform = `translateX(${distancia}%)`
+            })
+        }
+    
+    
+        avancar.addEventListener("click", () => {
+            i = (i + 1) % qtdCards;
+            moverCards();
+            console.log(i)
+        });
+    
+        
+        voltar.addEventListener("click", () => {
+            i = (i - 1 + qtdCards) % qtdCards;
+            moverCards();
+            console.log(i)
+        });
+
+    })
+
+
+})
