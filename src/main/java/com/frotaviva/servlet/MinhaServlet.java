e2dac9e0cfebf55a5e9f4de42574fb22e874be6c
RicardoCiriloJunior <ricardocjr07@gmail.com>
2025-09-24 16:14:51 -0300

    Adicionando o botão 'Área Restrita' na landing page que leva até a área de cadastro da empresa

--- src/main/java/com/frotaviva/servlet/MinhaServlet.java
+++ src/main/java/com/frotaviva/servlet/MinhaServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/minha-servlet")
+@WebServlet("/login")
 public class MinhaServlet extends HttpServlet {
 
     @Override
