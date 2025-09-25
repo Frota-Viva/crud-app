63b3baf6df75e8361decdf2dc20f78a5af6993db
RicardoCiriloJunior <ricardocjr07@gmail.com>
2025-09-25 16:53:46 -0300

    Fazendo o método para selecionar os registros da view 'perfilmotorista'

--- src/main/java/com/frotaviva/dao/FiltrosDAO.java
+++ src/main/java/com/frotaviva/dao/FiltrosDAO.java
@@ -11,26 +11,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Filtros {
-    public static List<Map<String, String>> caminhoesComMotoristas(){
+public class FiltrosDAO {
+    public static List<Map<String, String>> perfilMotorista(){
         List<Map<String, String>> motoristasCaminhoes = new ArrayList<>();
-        String sql = """
-                SELECT m.nome, c.placa FROM motorista m 
-                JOIN caminhao c 
-                ON m.id = c.id_motorista 
-                ORDER BY m.nome""";
+        String sql = "SELECT * FROM perfilmotorista"; //Selecionando a view de perfil do motorista
 
         try (Connection conn = Conexao.conectar();
-             PreparedStatement stmt = conn.prepareStatement(sql);){
+             PreparedStatement stmt = conn.prepareStatement(sql)){
             ResultSet rs = stmt.executeQuery();
 
             while (rs.next()){
-                String nome = rs.getString("nome");
                 String placa = rs.getString("placa");
+                String nome = rs.getString("nome");
+                String email = rs.getString("email");
+                String telefone_principal = rs.getString("Telefone_Principal");
+                String tipo_frota = rs.getString("tipo_frota");
 
                 Map<String, String> dados = new HashMap<>();
-                dados.put("nome", nome);
                 dados.put("placa", placa);
+                dados.put("nome", nome);
+                dados.put("email", email);
+                dados.put("telefone_principal", telefone_principal);
+                dados.put("tipo_frota", tipo_frota);
                 motoristasCaminhoes.add(dados);
             }
             return motoristasCaminhoes;
--- src/main/webapp/WEB-INF/view/login.jsp
+++ src/main/webapp/WEB-INF/view/login.jsp
@@ -7,11 +7,14 @@
 <body>
 <h2>Login</h2>
 <form action="login" method="post">
-    <label for="usuario">Usuário:</label>
-    <input type="text" id="usuario" name="usuario" required><br><br>
+    <label for="email">Digite o email da empresa:</label> <br>
+    <input type="text" id="email" name="usuario" placeholder="email@dominio.com" required><br><br>
 
-    <label for="senha">Senha:</label>
-    <input type="password" id="senha" name="senha" required><br><br>
+    <label for="cnpj">Digite o CNPJF da empresa:</label> <br>
+    <input type="text" id="cnpj" name="cnpj" placeholder="12.345.678/0001-11" required> <br><br>
+
+    <label for="senha">Digite a senha da empresa:</label> <br>
+    <input type="password" id="senha" name="senha" placeholder="*********" required><br><br>
 
     <button type="submit">Entrar</button>
 </form>
