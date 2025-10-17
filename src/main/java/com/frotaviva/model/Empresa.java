f9fb4084cc86327a1d41f49c1077e66e077f6afb
RicardoCiriloJunior <ricardocjr07@gmail.com>
2025-10-17 10:26:49 -0300

    feature(Cnpj): implementa o método que valida o cnpj e já o retorna validado

--- src/main/java/com/frotaviva/model/Empresa.java
+++ src/main/java/com/frotaviva/model/Empresa.java
@@ -19,6 +19,15 @@ public class Empresa {
         this.tipoEmpresa=tipoEmpresa;
         this.senha=senha;
     }
+    public Empresa(String tipoEmpresa,String cnpj,String email,String senha,String nome,
+                   Endereco endereco){
+        this.nome=nome;
+        this.email=email;
+        this.cnpj=cnpj;
+        this.endereco =endereco;
+        this.tipoEmpresa=tipoEmpresa;
+        this.senha=senha;
+    }
 
     public long getId(){
         return this.id;
@@ -52,6 +61,44 @@ public class Empresa {
     }
     public void setSenha(String senha){
         this.senha=senha;
+    }
+
+    public String CnpjValidado(String cnpj){
+        String regexCnpj = "^[0-9]{2}\\.?[0-9]{3}\\.?[0-9]{3}\\/?[0-9]{4}\\-?[0-9]{2}$"; //Regex para validar CNPJ
+
+        if (! cnpj.matches(regexCnpj)) return null;
+
+        cnpj = cnpj.replaceAll("[^0-9]", ""); //Pega apenas o número no CNPJ
+
+//        Verifica o tamanho do CNPJ e se ele apenas repete o mesmo número
+        if (cnpj.length() != 14 || cnpj.matches("^([0-9])\\1{13}$")) return null;
+
+        int[] pesos1 = {5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+        int[] pesos2 = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+
+        String base = cnpj.substring(0, 12);
+        String digitos = cnpj.substring(12);
+
+        int soma1 = 0;
+        int soma2 = 0;
+
+//        Soma todos os valores multiplicados pelos respectivos pesos
+        for (int i = 0; i < 12; i++) {
+            int num = Character.getNumericValue(base.charAt(i)); //Pega o valor numérico do caractere
+            soma1 += num * pesos1[i];
+            soma2 += num * pesos2[i];
+        }
+
+//        Calcula o 1° dígito
+        int resto = soma1 % 11;
+        int digito1 = (resto < 2) ? 0 : 11 - resto;
+
+//        Soma o digito1 multiplicado por seu peso e calcula o 2° dígito
+        soma2 += digito1 * pesos2[12];
+        resto = soma2 % 11;
+        int digito2 = (resto < 2) ? 0 : 11 - resto;
+
+        return digitos.equals("" + digito1 + digito2) ? cnpj : null;
     }
     public String toString(){
         return "Id: "+this.id+"\nNome: "+this.nome+"\nEndereco: "+this.endereco+"\nEmail: "+this.email+
